880124dde579c992daf1758df57fcdb3149c25f0
Omar Ahmed <109013442+MERNomar@users.noreply.github.com>
2023-05-23 11:42:51 +0300

    changing alot of sutff and alomst finshing the project

--- scripts/script.js
+++ scripts/script.js
@@ -2,61 +2,46 @@ const pcWin = "Computer wins";
 const humanWin = "Human wins";
 const tie = "Its a Tie !";
 let roundcounter =["One" , "Two" , "Three" , "four" , "Five"]
-let buttons = document.querySelectorAll(".button");
-
-
-
-
-
-
-const newgame = document.querySelector(".newgame")
-
-  
 let loseCounter = 0;
 let winCouneter = 0;
 let tieCounter = 0;
+let displyWin = document.querySelector(".winCounter")
+let displyLose = document.querySelector(".loseCounter")
+let displytie = document.querySelector(".tieCounter")
 
-
-
-
-
+//event lisnter that decides if its win or lose or tie every time i playround()
+let btns = document.querySelectorAll("[data-button]")
 // here afor loop named play round takes two parameters player and pc 
 // waiting 2 parametrs player and pc and its detarmnate the winner 
 const playRound = (player) => 
-{
+{  // here i got the pc choice 
   pcChocies = ["rock" , "papper" , "scissors" ]
-  
-  
  let pcnum = Math.floor(Math.random() * pcChocies.length) 
  let pc = pcChocies[pcnum]
-  
+
+
     
 
  console.log(`You picked ${player}`)
  console.log(`Your Computer picked ${pc}`)
 
-if ( player === pc ) {console.log(tie), tieCounter++}
-else if  (player === "rock" && pc === "papper") {console.log(pcWin) , loseCounter++ }
-else if  (player === "papper" && pc === "scissors") {console.log(pcWin) , loseCounter++}
-else if  (player === "scissors" && pc === "rock") {console.log(pcWin) , loseCounter++ }
+if ( player === pc ) {console.log(tie), tieCounter+= 1 , displytie.innerText = `ties = ${tieCounter}` }
+else if  (player === "rock" && pc === "papper") { console.log(pcWin) , loseCounter+= 1 , displyLose.innerText = `loses = ${loseCounter}` }
+else if  (player === "papper" && pc === "scissors") {console.log(pcWin) , loseCounter+= 1 , displyLose.innerText = `loses = ${loseCounter}`}
+else if  (player === "scissors" && pc === "rock") {console.log(pcWin) , loseCounter+= 1 , displyLose.innerText = `loses = ${loseCounter}`}
 
-else if  (pc === "rock" && player === "papper") {console.log(humanWin) , winCouneter++ }
-else if  (pc === "papper" && player === "scissors") {console.log(humanWin) , winCouneter++}
-else if  (pc === "scissors" && player === "rock") {console.log(humanWin) , winCouneter++ }
-
-
-else (console.log("man wtff ?"))}
+else if  (pc === "rock" && player === "papper") {console.log(humanWin) , winCouneter+= 1 , displyWin.innerText = `wins = ${winCouneter}` }
+else if  (pc === "papper" && player === "scissors") {console.log(humanWin) , winCouneter+= 1 , displyWin.innerText = `wins = ${winCouneter}`  }
+else if  (pc === "scissors" && player === "rock") {console.log(humanWin) , winCouneter+= 1 , displyWin.innerText = `wins = ${winCouneter}`}
 
 
-//function that takes rock or paper or scissors insted of prompting it 
-
-
+else (console.log("man wtff ?"))} 
 
 
 
 
 //here i made aloop thats call my playround function 5 times declare how is the winner !
- const game = () =>
+ const game = (player , ) =>
 {
   
  
@@ -65,7 +50,6 @@ else (console.log("man wtff ?"))}
    
   
   
-  let player = playerclicked;
   let pc = getComputerChoice();
   playRound(player , pc);
   console.log(`round ${roundcounter[i]}`)
@@ -79,39 +63,32 @@ else (console.log("man wtff ?"))}
 
 }
 
-function test(A)
-{
-if (A === 1){ console.log("one")}
-else if (A === 2){ console.log("two")}
-else if (A === 3){ console.log("three")}
-else {console.log("wtf")}
+const elements = document.querySelectorAll(".button")
 
+
+
+
+
+document.addEventListener('click', function(e){
+  
+  if(e.target.className == "rock"){ playRound("rock") 
+}else if (e.target.className == "paper"){playRound("papper") 
+}else if (e.target.className == "scissors") {playRound("scissors")
 }
-buttons.forEach((button) => {
-  button.addEventListener("click", () => {
-    const img = button.querySelector("img");
-    playerSelection = img.alt.toLowerCase();
 
-    playRound(playerSelection, getComputerChoice());
 
-    
-  });
-});
+})
 
 
 
-// let form = document.querySelector(".form")
-// let playerinput = document.querySelector(".playerchoice")
-// let playerbtn = document.querySelector("#letsplay")
-// let playerclicked = playerbtn.addEventListener("click" , e => {e.preventDefault()
-//  playerinput.value
-// })
+// btns.forEach(buttons =>(buttons.addEventListener("click" , e=> {
+//   winCouneter+=1
+//   displyWin.innerText = `wins = ${winCouneter}`
+//   displyLose.innerText = `loses = ${loseCounter}`
+//   displytie.innerText = `ties = ${tieCounter}`
+  
+ 
+  
 
+// })))
 
-
-
-
-
-
-
-playRound("rock")
